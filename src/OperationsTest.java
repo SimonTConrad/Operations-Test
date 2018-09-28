a50d5995f65fa40dc59737b98653954f46a6ff45
Kronos <42975620+SimonTConrad@users.noreply.github.com>
2018-09-28 12:27:42 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,21 @@
+
+public class OperationsTest {
+
+	public static void main(String[] args) 
+	{
+		int classic; //Declaration statement
+		classic = 6; //Assignment statement -> initialization
+		classic = classic + 5; // number+=5 
+		System.out.println(classic);
+		classic/=2;
+		System.out.println(classic);
+		classic-=6;
+		System.out.println(classic);
+		classic*=4;
+		System.out.println(classic);
+		classic%=2;
+		System.out.println(classic);
+
+	}
+
+}
